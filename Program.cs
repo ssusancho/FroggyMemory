def8cbc191264abd49f5af27e328f1b7c43d718b
ssusancho <scho2024@chadwickschool.org>
2022-02-16 12:10:05 -0800

    chore: Initialize Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FroggyMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
